d6ab8fbb96bce956b04cd68a2ffe52d85bc87e9d
srijnasri <srijnasri1999@gmail.com>
2024-03-12 09:53:10 +0530

    Initial Commit

---
+++ clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clima
+//
+//  Created by Srijnasri Negi on 12/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
